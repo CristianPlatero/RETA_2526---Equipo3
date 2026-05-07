3c57a69dacbb9e0cc69e757b8017c96f1848df19
RFCDAW1 <rfernandezc03@educantabria.es>
2026-05-07 17:05:29 +0200

    cambio en diagrama E/R y en la BD
    Co-Authored-By: tiimurr <190379387+tiimurr@users.noreply.github.com>

--- Bases De Datos/inventario_taller.sql
+++ Bases De Datos/inventario_taller.sql
@@ -59,13 +59,19 @@ descripcion varchar(150) not null,
 categoria enum('portatil', 'sobremesa') not null,
 estado enum('operativo','averiado','en reparación', 'obsoleto') not null DEFAULT 'operativo',
 cantidad int not null,
-id_estacion int not null,
-fecha_alta DATE DEFAULT (current_date),
+id_estacion int null,
+id_armario int null,
+id_balda int null,
+fecha_alta DATETIME not null DEFAULT (CURRENT_DATE),
 observaciones varchar(150) null,
 
 PRIMARY KEY (id_pc),
-FOREIGN KEY (id_ubi) REFERENCES ubicacion(id_ubi),
+FOREIGN KEY (id_estacion) REFERENCES estacion(id_estacion),
+FOREIGN KEY (id_armario) REFERENCES armario(id_armario),
+FOREIGN KEY (id_balda) REFERENCES balda(id_balda),
+
 CHECK (cantidad >= 0)
+
 );
 
 DROP TABLE if exists componentes;
@@ -75,14 +81,22 @@ nombre varchar(50) not null,
 descripcion varchar(150) not null,
 estado enum('operativo','averiado','en reparación', 'obsoleto') not null DEFAULT 'operativo',
 cantidad int not null,
+
 id_armario int,
+id_estacion int null,
+id_balda int null,
 id_pc int,
+
 fecha_alta DATE DEFAULT (current_date),
 observaciones varchar(150),
 
 PRIMARY KEY (id_comp),
 FOREIGN KEY (id_armario) REFERENCES armario(id_armario),
+FOREIGN KEY (id_estacion) REFERENCES estacion(id_estacion),
+FOREIGN KEY (id_balda) REFERENCES balda(id_balda),
+
 FOREIGN KEY (id_pc) REFERENCES pcs(id_pc),
+
 CHECK (cantidad >= 0)
 
 );
@@ -95,13 +109,19 @@ nombre varchar(50) not null,
 descripcion varchar(150) not null,
 estado enum('operativo','averiado','en reparación', 'obsoleto') not null DEFAULT 'operativo',
 cantidad int not null,
+
 id_estacion int,
+id_armario int null,
+id_balda int null,
 id_pc int not null,
+
 fecha_alta DATE DEFAULT (current_date),
 observaciones varchar(150),
 
 PRIMARY KEY (id_peri),
 FOREIGN KEY (id_estacion) REFERENCES estacion(id_estacion),
+FOREIGN KEY (id_armario) REFERENCES armario(id_armario),
+FOREIGN KEY (id_balda) REFERENCES balda(id_balda),
 FOREIGN KEY (id_pc) REFERENCES pcs(id_pc),
 
 CHECK (cantidad >= 0)
@@ -115,14 +135,99 @@ nombre varchar(50) not null,
 descripcion varchar(150) not null,
 estado enum('operativo','averiado','en reparación', 'obsoleto') not null DEFAULT 'operativo',
 cantidad int not null,
+
 id_estacion int,
 id_armario int,
+id_balda int null,
+
 fecha_alta DATE DEFAULT (current_date),
 observaciones varchar(150),
 
 PRIMARY KEY (id_red),
 FOREIGN KEY (id_estacion) REFERENCES estacion(id_estacion),
 FOREIGN KEY (id_armario) REFERENCES armario(id_armario),
+FOREIGN KEY (id_balda) REFERENCES balda(id_balda),
+
+CHECK (cantidad > 0)
+);
+
+
+
+
+DROP TABLE if exists cableado;
+CREATE TABLE if not exists cableado(
+id_cable int not null auto_increment,
+nombre varchar(50) not null,
+descripcion varchar(150) not null,
+estado enum('operativo','averiado','en reparación', 'obsoleto') not null DEFAULT 'operativo',
+cantidad int not null,
+
+id_pc int,
+id_estacion int,
+id_armario int,
+id_balda int null,
+
+fecha_alta DATE DEFAULT (current_date),
+observaciones varchar(150),
+longitud int not null,
+conector1 varchar(50) not null,
+conector2 varchar(50) not null,
+
+PRIMARY KEY (id_cable),
+FOREIGN KEY (id_estacion) REFERENCES estacion(id_estacion),
+FOREIGN KEY (id_armario) REFERENCES armario(id_armario),
+FOREIGN KEY (id_balda) REFERENCES balda(id_balda),
+FOREIGN KEY (id_pc) REFERENCES pcs(id_pc),
+
+CHECK (cantidad > 0)
+);
+
+
+
+
+DROP TABLE if exists herramientas;
+CREATE TABLE if not exists herramientas(
+id_her int not null auto_increment,
+nombre varchar(50) not null,
+descripcion varchar(150) not null,
+estado enum('operativo','averiado','en reparación', 'obsoleto') not null DEFAULT 'operativo',
+cantidad int not null,
+
+id_estacion int,
+id_armario int,
+id_balda int null,
+
+fecha_alta DATE DEFAULT (current_date),
+observaciones varchar(150),
+
+PRIMARY KEY (id_her),
+FOREIGN KEY (id_estacion) REFERENCES estacion(id_estacion),
+FOREIGN KEY (id_armario) REFERENCES armario(id_armario),
+FOREIGN KEY (id_balda) REFERENCES balda(id_balda),
+
+CHECK (cantidad > 0)
+);
+
+
+DROP TABLE if exists material_fungible;
+CREATE TABLE if not exists material_fungible(
+id_matF int not null auto_increment,
+nombre varchar(50) not null,
+descripcion varchar(150) not null,
+estado enum('operativo','averiado','en reparación', 'obsoleto') not null DEFAULT 'operativo',
+cantidad int not null,
+
+id_estacion int,
+id_armario int,
+id_balda int null,
+
+fecha_alta DATE DEFAULT (current_date),
+observaciones varchar(150),
+
+PRIMARY KEY (id_matF),
+FOREIGN KEY (id_estacion) REFERENCES estacion(id_estacion),
+FOREIGN KEY (id_armario) REFERENCES armario(id_armario),
+FOREIGN KEY (id_balda) REFERENCES balda(id_balda),
 
 CHECK (cantidad > 0)
 );
