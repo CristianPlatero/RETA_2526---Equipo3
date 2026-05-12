a104e75cd29b09bfb3114114c2a772fcf516cdab
CristianPlatero <cplateroz2501@educantabria.es>
2026-05-12 15:55:35 +0200

    interfaz java
    plantilla básica para Swing
    
    Co-Authored-By: Ossel18 <246123098+Ossel18@users.noreply.github.com>

---
+++ java/Reta2526_Equipo_3/src/main/java/com/mycompany/reta2526_equipo_3/App.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.reta2526_equipo_3;
+
+import Interfaz.Marco;
+
+/**
+ *
+ * @author DAW102
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Marco miMarco = new Marco();
+    }
+    
+}
